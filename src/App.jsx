71fd9918124cb347d25b19ff9bdb15e1bc265c5a
Anjum <anjumshaikh152535@gmail.com>
2024-07-06 14:48:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <p className="text-red-400">Hello world</p>;
+  return (
+    <>
+      <p className="text-red-400">Hello world</p>
+      <h1>hi</h1>
+    </>
+  );
 }
 
 export default App;
